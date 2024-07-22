d1b063804a9e6dc731a32089a7a4b8d87d299d6c
israel zambrana <israelzambrana@gmail.com>
2024-07-21 22:26:50 -0400

    Add CRUD tests for the user endpoint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   defaultCommandTimeout:20000,
   e2e: {
     baseUrl:"https://petstore.swagger.io/v2",
